74dd2a07e0e4134114d97a2079f21300bf0f4fb0
David Henley <henleydr@gmail.com>
2017-06-22 10:16:40 -0500

    Init Commit

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var first = 15
+var second = 5
+var third = first + second
+var fourth = 5 * 9
+var fifth = first - second
+var sixth = first / second
+
+var a = 7, b = 3, c = 4
+
+let d = 4, e = 1, f = 3
+
+var double: Double
+var int: Int = 423
+
+var hello = "Hello"
+var world = "World"
+
+var welcome = hello + " " + world
+var welcome2 = "\(hello) \(world)"
+
